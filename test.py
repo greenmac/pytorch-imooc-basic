dfcbd1a7381cff0b406f23dac591ac4ff4aab54c
greenmac <alwaysmac@msn.com>
2020-01-21 11:34:47 +0800

    4.2_CNN_Classification

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 lst = [1, 2, 3]
-print(type(lst))
-print(type(iter(lst)))
+# print(type(lst))
+# print(type(iter(lst)))
 # for i in iter(lst):
 #     print(i)
+
+dict_words = {'Google': 'www.google.com', 'Runoob': 'www.runoob.com', 'taobao': 'www.taobao.com'}
+# print(dict_words.items())
+
+# for key in dict_words:
+#     print(key)
+
+# for key, value in dict_words.items():
+    # print(key, ':', value)
